d38394c6613c48ff424fa29c52d5c52cd520fb9d
Lala Arnav Vatsal <lalaarnav003@gmail.com>
2023-08-21 17:32:50 +0530

    Update 0.1.3

--- Backend/diabetes_model.py
+++ Backend/diabetes_model.py
@@ -5,17 +5,22 @@ from sklearn import svm
 from sklearn.metrics import accuracy_score
 import pickle
 
-diabetes_dataset = pd.read_csv('Dataset/diabetes.csv')
+diabetes_dataset = pd.read_csv('Dataset/diabetes_new.csv')
 
-X = diabetes_dataset.drop(columns='Outcome', axis=1)
-Y = diabetes_dataset['Outcome']
+X = diabetes_dataset.drop(columns=['CLASS', 'No_Pation', 'ID'], axis=1)
+Y = diabetes_dataset['CLASS']
+
+gender_dict = {'M': 0, 'F': 1}
+for i, ele in enumerate(X['Gender']):
+    X['Gender'][i] = gender_dict[ele.strip().upper()]
+
+class_dict = {'Y': 0, 'P': 1, 'N': 2}
+for i, ele in enumerate(Y):
+    Y[i] = class_dict[ele.strip().upper()]
 
 scaler = StandardScaler()
-scaler.fit(X)
-standardized_data = scaler.transform(X)
-
-X = standardized_data
-Y = diabetes_dataset['Outcome']
+X = scaler.fit_transform(X)
+Y=Y.astype('int')
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
 
@@ -28,4 +33,6 @@ training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
 X_test_prediction = classifier.predict(X_test)
 test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
 
-pickle.dump(classifier, open("diabetes_model.pkl", 'wb'))
+print('Accuracy: ', test_data_accuracy)
+
+pickle.dump(classifier, open("Models/diabetes_model_new.pkl", 'wb'))
--- Frontend/pages/diabetes.py
+++ Frontend/pages/diabetes.py
@@ -6,47 +6,59 @@ from sklearn.preprocessing import StandardScaler
 
 
 def predict(input_data):
-    input_data_as_numpy_array = np.asarray(input_data)
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    inp = np.asarray(input_data).reshape(1, -1)
 
-    diabetes_dataset = pd.read_csv('Dataset/diabetes.csv')
-    X = diabetes_dataset.drop(columns='Outcome', axis=1)
+    diabetes_dataset = pd.read_csv('Dataset/diabetes_new.csv')
+    X = diabetes_dataset.drop(columns=['CLASS', 'No_Pation', 'ID'], axis=1)
+
+    gender_dict = {'M': 0, 'F': 1}
+    for i, ele in enumerate(X['Gender']):
+        X['Gender'][i] = gender_dict[ele.strip().upper()]
 
     scaler = StandardScaler()
     scaler.fit(X)
-    std_data = scaler.transform(input_data_reshaped)
+    std_data = scaler.transform(inp)
 
-    data_df = pd.DataFrame(data=std_data, columns=['Pregnancies', 'Glucose', 'BloodPressure',
-                                                   'SkinThickness', 'Insulin', 'BMI',
-                                                   'DiabetesPedigreeFunction', 'Age'])
+    classifier = pickle.load(open('Models/diabetes_model_new.pkl', "rb"))
+    prediction = classifier.predict(std_data)[0]
 
-    classifier = pickle.load(open('Models/diabetes_model.pkl', "rb"))
-    prediction = classifier.predict(data_df)
-
-    if prediction[0] == 0:
-        return 'not diabetic'
-    else:
+    if prediction == 0:
         return 'diabetic'
+    elif prediction == 1:
+        return 'pre-diabetic'
+    elif prediction == 2:
+        return 'not diabetic'
 
 
 st.markdown('# Diabetes Prediction')
-st.markdown('*The model used here is trained on a dataset of females at least 21 years old of Pima Indian heritage.*')
+st.markdown('*The model used here is trained on a dataset containing medical information and laboratory analysis of '
+            'Iraqi patients. This data was acquired from the laboratory of Medical City Hospital and the Specialized '
+            'Center for Endocrinology and Diabetes (Al-Kindi Teaching Hospital).*')
 
-parameters = ['Pregnancies', 'Glucose', 'Blood Pressure',
-              'Skin Thickness', 'Insulin', 'BMI',
-              'Diabetes Pedigree Function', 'Age']
-parameters_help = ['Number of times pregnant',
-                   'Plasma glucose concentration a 2 hours in an oral glucose tolerance test',
-                   'Diastolic blood pressure (mm Hg)',
-                   'The thickness of the skin fold on the triceps area of the arm (mm)',
-                   'The level of insulin in the blood 2 hours after a meal (mu U/ml)',
-                   'Body mass index (weight in kg/(height in m)^2)',
-                   'A function that quantifies the likely hereditary risk of diabetes based on family history.',
-                   'Age (years)']
-input_data = [5, 166, 72, 19, 175, 25.8, 0.587, 51]
+parameters = ['Age', 'Urea', 'Creatinine ratio', 'HBA1C',
+              'Cholesterol', 'Triglycerides', 'HDL', 'LDL',
+              'VLDL', 'Body Mass Index']
+
+parameters_help = ['**Your age in years.**',
+                   '**Level of urea in your blood.** Normal Range: 5 to 20 mg/dl',
+                   '**The ratio of creatinine in your urine to the creatinine level in your blood.** Normal Range: 0.74 to 1.35 mg/dL [men]; 0.59 to 1.04 mg/dL [women]',
+                   '**Your average blood glucose (sugar) levels for the last two to three months.** Normal Range: 4% to 5.6%',
+                   '**Your total cholesterol level, which is a fatty substance found in your blood.** Normal Range: 125 to 200mg/dL',
+                   '**Your triglyceride level, which is a type of fat found in your blood.** Normal Range: Less than 150 mg/dL',
+                   '**High-Density Lipoprotein, often referred to as "good" cholesterol.** Normal Range: 35 to 65 mg/dL [men], 35 to 80 mg/dL [women]',
+                   '**Low-Density Lipoprotein, often referred to as "bad" cholesterol.** Normal Range: Less than 100mg/dL',
+                   '**Very Low-Density Lipoprotein, which is a type of lipoprotein that carries triglycerides in the blood.** Normal Range: 2 to 30 mg/dL',
+                   '**Body mass index (weight in kg/(height in m)^2).** Normal Range: 18.5 to 24.9']
+
+# input_data = [1, 50, 4.7, 46, 4.9, 4.2, 0.9, 2.4, 1.4, 0.5, 24.0]
+input_data = [0, 34, 3.9, 81, 6, 6.2, 3.9, 0.8, 1.9, 1.8, 23]
+
+input_data[0] = {'Male': 0, 'Female': 1}[
+    st.selectbox(label='Gender', options=['Male', 'Female'], index=1, help='Enter your gender')]
+
 for i, parameter in enumerate(parameters):
-    input_data[i] = st.text_input(label=parameter, value=input_data[i], help=parameters_help[i])
+    input_data[i + 1] = st.text_input(label=parameter, value=input_data[i + 1], help=parameters_help[i])
 
 if st.button("Predict"):
     prediction = predict(input_data)
-    st.write(f'The person is **{prediction}**')
+    st.markdown(f'# The person is {prediction}')
